af1ff6c332a59f02399e1d536b7c3587a3ecaaaa
RYU <takahashi.ryunosuke.job@gmail.com>
2024-11-30 17:15:32 +0900

    🚀

--- src/features/auth/sign-up-button.tsx
+++ src/features/auth/sign-up-button.tsx
@@ -5,6 +5,7 @@ import { Button } from '~/components/ui'
 export default function SignUpButton() {
   return (
     <Button
+      // eslint-disable-next-line no-alert
       onPress={() => alert('Sign Up!')}
       type="button"
     >
